bdc44632396156b909dc5e9da4ed8fa847a91131
amishtp1407 <amish@haltdos.com>
2022-10-12 22:01:40 +0530

    f

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -15,9 +15,9 @@ const config = {
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
   url: 'https://halt-dos.github.io',
-  baseUrl: '/wiki/',
+  baseUrl: '/wiki-base/',
   organizationName: 'haltdos', // Usually your GitHub org/user name.
-  projectName: 'wiki', // Usually your repo name.
+  projectName: 'wiki-base', // Usually your repo name.
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
